bea798631572fe6aa6991f8328cd80a974198998
kat <kat@pop-os.localdomain>
2023-12-04 15:36:09 +0000

    Add test to verify POST requests to /api/blogs work.

--- tests/blog-api.test.js
+++ tests/blog-api.test.js
@@ -14,16 +14,41 @@ beforeEach(async () => {
   await Promise.all(promiseArray);
 });
 
-// GET
-test('/api/blogs returns the correct amount of posts', async () => {
-  const response = await api.get('/api/blogs');
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+// POST
+describe('POST', () => {
+  test('A new blog can be POSTed', async () => {
+    const newBlog = {
+      title: 'Why Can\'t I Evolve?',
+      author: 'Piplup',
+      url: 'www.pochama.jp/let-me-evolve',
+      likes: 393
+    };
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogs = await helper.jsonBlogsInDb();
+    expect(blogs.length).toEqual(helper.initialBlogs.length + 1);
+
+    const urls = blogs.map((blog) => blog.url);
+    expect(urls).toContain(newBlog.url);
+  });
 });
 
-test('`__id` property is changed to `id`', async () => {
-  const blogs = await helper.jsonBlogsInDb();
-  expect(blogs[0].id).toBeDefined();
-  expect(blogs[0].__id).toBeUndefined();
+// GET
+describe('GET', () => {
+  test('/api/blogs returns the correct amount of posts', async () => {
+    const response = await api.get('/api/blogs');
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test('`__id` property is changed to `id`', async () => {
+    const blogs = await helper.jsonBlogsInDb();
+    expect(blogs[0].id).toBeDefined();
+    expect(blogs[0].__id).toBeUndefined();
+  });
 });
 
 afterAll(async () => {
